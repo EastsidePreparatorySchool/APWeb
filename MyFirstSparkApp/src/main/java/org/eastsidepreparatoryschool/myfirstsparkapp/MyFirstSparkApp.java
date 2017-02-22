fdb55089a4fdc2fd1ab53832caf9bc41cfe6c503
Gunnar Mein <gmein@eastsideprep.org>
2017-02-22 12:20:54 -0800

    Spark project and template app

---
+++ MyFirstSparkApp/src/main/java/org/eastsidepreparatoryschool/myfirstsparkapp/MyFirstSparkApp.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.eastsidepreparatoryschool.myfirstsparkapp;
+
+import static spark.Spark.*;
+
+/**
+ *
+ * @author gmein
+ */
+public class MyFirstSparkApp {
+
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+
+}
