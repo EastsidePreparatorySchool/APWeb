1a563a6ea3497c95ef0fe586b347a33ac1d0ce3f
elusch21 <eschreiber@eastsideprep.org>
2017-04-18 19:56:29 -0700

    Version 2.0
    Submit-Ready version

--- ChatServerES/src/main/java/org/eastsideprep/chatserveres/ChatServerES.java
+++ ChatServerES/src/main/java/org/eastsideprep/chatserveres/ChatServerES.java
@@ -5,54 +5,73 @@
  */
 package org.eastsideprep.chatserveres;
 
-import static spark.Spark.get;
-import static spark.Spark.staticFiles;
-import static spark.Spark.put;
+import static spark.Spark.*;
 import java.util.*;
+import javax.servlet.MultipartConfigElement;
 
 /**
  *
  * @author ESchreiber
  */
 public class ChatServerES {
+
     static ArrayList<String> messages = null;
+
     public static void main(String[] args) {
         messages = new ArrayList<String>();
         staticFiles.location("/static");
-        put("/putmessage", (req, res) -> putMessage(req));
-        get("/getnewmessages", (req, res) -> getNewMessages(req, res));
-        get("/hello", (req, res) -> syncTest());
-        get("/Sandwich", (req, res) -> "Que Interesante");
-        get("/shutdown", (req, res) -> {System.exit(0); return 0;});
+        put("/protected/putmessage", (req, res) -> putMessage(req));
+        get("/protected/getnewmessages", (req, res) -> getNewMessages(req, res));
+        post("/protected/postmessage", (req, res) -> postMessage(req));
+        before("/protected/*", (req, res) -> {
+            if (req.session().attribute("initials") == null) {
+                halt(401, "I require login");
+            }
+        });
+        put("/login", (req, res) -> login(req));
     }
+
     public static boolean putMessage(spark.Request request) {
-        messages.add(request.session().id()+": "+request.body());
-        return true;
+        synchronized (messages) {
+            messages.add(request.session().attribute("initials") + ": " + request.body());
+            return true;
+        }
     }
+
+    public static String postMessage(spark.Request req) {
+        synchronized (messages) {
+            Context ctx = getCtxFromSession(req.session());
+            MultipartConfigElement multipartConfigElement = new MultipartConfigElement(System.getProperty("java.io.tmpdir"));
+            req.raw().setAttribute("org.eclipse.jetty.multipartConfig", multipartConfigElement);
+            messages.add(req.session().attribute("initials") + ":" + req.queryParams("message"));
+            return req.session().id();
+        }
+    }
+
+    private static String login(spark.Request req) {
+        req.session().attribute("initials", req.body());
+        return req.body();
+    }
+
     public static String getNewMessages(spark.Request request, spark.Response response) {
-        Context ctx = getCtxFromSession(request.session());
-        StringBuilder result = new StringBuilder();
-        for(int i = ctx.seen; i <messages.size(); i++) {
-            String s = messages.get(i);
-            if (s.startsWith(request.session().id())) {
-                result.append(s.substring(request.session().id().length() + 1));
+        synchronized (messages) {
+            Context ctx = getCtxFromSession(request.session());
+            StringBuilder result = new StringBuilder();
+            for (int i = ctx.seen; i < messages.size(); i++) {
+                String s = messages.get(i);
+                if (s.startsWith(request.session().attribute("initials"))) {
+                    result.append(s.substring(request.session().attribute("initials").toString().length() + 1));
+                } else {
+                    result.append("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+                    result.append(s);
+                }
+                result.append("<br>");
             }
-            else {
-                result.append("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
-                result.append(s);
-            }
-            result.append("<br>");
+            ctx.seen = messages.size();
+            return result.toString();
         }
-        ctx.seen = messages.size();
-        return result.toString();
     }
-    public static String syncTest() {
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException ex) {
-        }
-        return "Hello World";
-    }
+
     public static Context getCtxFromSession(spark.Session s) {
         Context ctx = s.attribute("Context");
         if (ctx == null) {
