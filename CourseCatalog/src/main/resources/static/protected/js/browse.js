2201bae979a2ca2d58f2dade2a6ae8c36c251b0c
Triple60 <asharma@eastsideprep.org>
2017-05-12 11:01:09 -0700

    Preliminary changes
    I have to pass in the parameters to Ethan's program.

--- CourseCatalog/src/main/resources/static/protected/js/browse.js
+++ CourseCatalog/src/main/resources/static/protected/js/browse.js
@@ -1,2 +1,15 @@
-//	• Return value from radio button in the req.body to the Java function as a URL
-//		○ Admin_course.js, pass in as URL
+function getSpecificCourseOfferings(discipline, gradeLevel, courseLength) {
+    //document.getElementById("courseInput").value = "";
+    var courseID = document.getElementById("courseInput").value;
+    output("requesting students for course ..." + courseID);
+
+    request({url: "/protected/admin/getSpecificCourseOfferings?id="+courseID})
+            .then(data => {
+                output("success");
+                eraseDiv("courseOutput");
+                insertStudents("courseOutput", data);
+            })
+            .catch(error => {
+                output("Error: " + error);
+            });
+}
